cba35a4b48ce6b8c07d19b990031bd0946115052
putztzu <tonysu.su-networking.com>
2014-04-01 22:55:26 -0700

    Upload working scripts and modify README

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 openSUSE_Oracle_Java_Install
 ============================
+Scripts to install Oracle Java into openSUSE
+Until this README sez differently, the scripts are a work in progress.
 
-Scripts that install Oracle Java JDK into openSUSE easily
+Requirements:
+-------------
+openSUSE 32bit or 64bit
+Although scripts were created on 13.1, the scripts likely work on any version of openSUSE
+
+Instructions:
+-------------
+Download this repository. If git is not already installed on your machine, the following command installs
+    zypper in git
+
+After git is installed, you can clone this repository(create a copy on your local machine) with the following command so you have a local copy of the scripts
+
+    git clone https://github.com/putztzu/openSUSE_Oracle_Java_Install.git
+
+The scripts are based on an Oracle JDK RPM installation. If a TAR is used instead, paths would need to be modified, so is highly discouraged.
+
+
+The Oracle JDK RPM can be downloaded from 
+http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html
+
+Install the RPM with the following command
+    zypper in jdk*
+
+Then execute the scripts in their numbered order.
+
+Scripts up to and including 5 <work> without a problem AFAIK.
+
+Based on the original instructions at
+-------------------------------------
+http://en.opensuse.org/SDB:install_java
+
+
+
+
+
+Testing needed.
---
+++ i586/3.Prepare_directory_for_jvm-expoerts.sh
@@ -0,0 +1,31 @@
+mkdir /usr/lib/jvm-exports/jdk_Oracle &&
+cd /usr/lib/jvm-exports/jdk_Oracle &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/rt.jar jaas-1.7.0_Orac.jar &&
+ln -s jaas-1.7.0_Orac.jar jaas-1.7.0.jar &&
+ln -s jaas-1.7.0_Orac.jar jaas.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/jce.jar jce-1.7.0_Orac.jar &&
+ln -s jce-1.7.0_Orac.jar jce-1.7.0.jar &&
+ln -s jce-1.7.0_Orac.jar jce.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/rt.jar jdbc-stdext-1.7.0_Orac.jar &&
+ln -s jdbc-stdext-1.7.0_Orac.jar jdbc-stdext-1.7.0.jar &&
+ln -s jdbc-stdext-1.7.0_Orac.jar jdbc-stdext-3.0.jar &&
+ln -s jdbc-stdext-1.7.0_Orac.jar jdbc-stdext.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/rt.jar jndi-1.7.0_Orac.jar &&
+ln -s jndi-1.7.0_Orac.jar jndi-1.7.0.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/rt.jar jndi-cos-1.7.0_Orac.jar &&
+ln -s jndi-cos-1.7.0_Orac.jar jndi-cos-1.7.0.jar &&
+ln -s jndi-cos-1.7.0_Orac.jar jndi-cos.jar &&
+ln -s jndi-1.7.0_Orac.jar jndi.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/rt.jar jndi-ldap-1.7.0_Orac.jar &&
+ln -s jndi-ldap-1.7.0_Orac.jar jndi-ldap-1.7.0.jar &&
+ln -s jndi-ldap-1.7.0_Orac.jar jndi-ldap.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/rt.jar jndi-rmi-1.7.0_Orac.jar &&
+ln -s jndi-rmi-1.7.0_Orac.jar jndi-rmi-1.7.0.jar &&
+ln -s jndi-rmi-1.7.0_Orac.jar jndi-rmi.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/jsse.jar jsse-1.7.0_Orac.jar &&
+ln -s jsse-1.7.0_Orac.jar jsse-1.7.0.jar &&
+ln -s jsse-1.7.0_Orac.jar jsse.jar &&
+ln -s /usr/lib/jdk_Oracle/jre/lib/rt.jar sasl-1.7.0_Orac.jar &&
+ln -s sasl-1.7.0_Orac.jar sasl-1.7.0.jar &&
+ln -s sasl-1.7.0_Orac.jar sasl.jar &&
+echo "Done"
